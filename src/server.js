569d711654f932b71a5b85c51beb322b0180786e
Robson Santiago <robsonsanoliveira@gmail.com>
2025-09-25 17:54:00 -0300

    Alterado o server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível em http://localhost:8000")
+  console.log("Servidor iniciado na porta 8000")
+  console.log("API disponível através da URLhttp://localhost:8000")
 })
